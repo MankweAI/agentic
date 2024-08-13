0b5075e5fe8282a9b9ef7cd739af34d30ed3a71a
AIDentistry <zettaverse86@gmail.com>
2024-08-13 14:20:20 +0200

    Modified code for testing

--- src/components/infobar/index.tsx
+++ src/components/infobar/index.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import BreadCrumb from "./bread-crumb";
 import { Card } from "../ui/card";
-import { DeleteChatroom} from "./deleteChatroom";
+import { DeleteChatroom } from "./deleteChatroom";
 import { Headphones, Star, Trash } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import {
@@ -12,37 +12,38 @@ import {
   deleteChatRoom,
 } from "@/hooks/conversation/use-conversation";
 import useSideBar from "@/context/use-sidebar";
+import { useConversation } from "@/hooks/conversation/use-conversation";
 import { useState } from "react";
 
 type Props = {};
 
 const InfoBar = (props: Props) => {
+  // const { onChangeStarredStatus } = useConversation();
+
   const { chatRoom, loading } = useSideBar();
   const tempChatRoom = chatRoom ? chatRoom : null;
   const [chatRoomStarred, setChatRoomStarred] = useState<boolean | null>(null);
   const [starClicked, setStarClicked] = useState<boolean>(false);
-  // console.log("chatRoom is null or undefined", tempChatRoom);
+  // console.log("chatRoom is null or undefined", domainId);
 
   if (chatRoom !== null && chatRoom !== undefined) {
     if (loading) {
       getChatRoomStar(chatRoom).then((res) => {
         setChatRoomStarred(res?.starred ?? null);
       });
-    } 
+    }
   }
 
   const handleStarClick = () => {
-    chatRoom && setChatRoomStarred((prev) => !prev);
-    chatRoom && updateChatroomStar(chatRoom, !chatRoomStarred);
-    setStarClicked(true);
+    // chatRoom && onChangeStarredStatus(chatRoom, false)
+    // chatRoom && updateChatroomStar(chatRoom, !chatRoomStarred);
+    // setStarClicked(true);
   };
 
-  // Handle the delete chatroom. Pop up a modal to confirm the delete action before deleting the chatroom 
+  // Handle the delete chatroom. Pop up a modal to confirm the delete action before deleting the chatroom
   const handleTrashClick = () => {
     chatRoom && deleteChatRoom(chatRoom);
   };
-
-
 
   return (
     <div className="flex w-full justify-between items-center py-1 mb-8 ">
